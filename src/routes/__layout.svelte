17c147fa890f91320cb2d44363ac3800b8497b29
anand2312 <anandkrishna2312@gmail.com>
2022-01-04 19:27:57 +0400

    fix: colors work now!

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,6 +8,6 @@
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,200;0,400;1,200&display=swap" rel="stylesheet">
 </svelte:head>
 
-<body class="primary-50">
+<div class="h-screen bg-primary-800">
     <slot />
-</body>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <title>Diary</title>
 
-<center class="flex h-screen">
-        <div class="m-auto">
-            <h1 class="font-display text-5xl italic hover:not-italic">pls</h1>
-            <br>
-            <p class="font-body text-xl">why no bg</p>
+<center class="flex h-screen justify-between">
+    <div class="m-auto">
+        <h1 class="font-display text-5xl italic hover:not-italic text-red-300 hover:text-gray-50">Diary</h1>
+        <br>
+        <p class="font-body text-xl text-amber-200">How was your day?</p>
     </div>
 </center>
