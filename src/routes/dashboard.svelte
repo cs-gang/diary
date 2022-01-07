acd810cd5517d9a93e35f4ec64ae102daf6e172f
anand2312 <anandkrishna2312@gmail.com>
2022-01-07 12:46:36 +0400

    fix: use another favicon

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -16,8 +16,15 @@
 </script>
 
 <main>
+    <title>Dashboard</title>
     {#if !user.discordUser}
-        <p class="font-body text-gray-50">loading</p>
+        <center class="h-screen justify-between">
+            <p class="text-gray-50 font-display text-5xl">Loading...</p>
+            <p class="text-gray-50 font-display text-xl animate-pulse">
+                Psst if you've been stuck here for too long, you've probably not logged in.<br>
+                I know I should probably redirect you to the login page, but right now I'm too lazy.
+            </p>
+        </center>
     {:else}
         <Navbar username={user.discordUser.username} avatarUrl={foo()} />
     {/if}
