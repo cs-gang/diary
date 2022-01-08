95678610be464ee5c7f498d063a946a4fb40b13e
anand2312 <anandkrishna2312@gmail.com>
2022-01-08 11:55:25 +0400

    chore: add meta tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,6 +10,8 @@
 		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,200;0,400;1,200&display=swap"
 		rel="stylesheet"
 	/>
+	<meta property="og:title" content="Diary" />
+	<meta property="og:description" content="Write about your day" />
 </svelte:head>
 
 <div class="h-screen bg-primary-600">
