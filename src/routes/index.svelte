2bc313d1cf3a56c4637fe8b3ea98a9dbe1b2167f
Windsmith <shriramsekar11@gmail.com>
2022-04-22 20:28:45 +0400

    feat: make text letter hopping animation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,7 +38,22 @@
         Diary
       </h1>
       <br />
-      <p class="font-body text-2xl md:text-3xl text-amber-200 italic">How was your day?</p>
+      <p class="font-body text-2xl md:text-3xl text-amber-200 italic waviy mt-2">
+        <span style="--i:1">H</span>
+        <span style="--i:2">o</span>
+        <span style="--i:3">w</span>
+        <span style="--i:4" class="ml-6">w</span>
+        <span style="--i:5">a</span>
+        <span style="--i:6">s</span>
+        <span style="--i:7" class="ml-6">y</span>
+        <span style="--i:8">o</span>
+        <span style="--i:9">u</span>
+        <span style="--i:10">r</span>
+        <span style="--i:11" class="ml-6">d</span>
+        <span style="--i:12">a</span>
+        <span style="--i:13">y</span>
+        <span style="--i:14">?</span>
+      </p>
       <br /><br />
       <small
         ><p class="font-body text-lg text-gray-100 animate-pulse mt-8 md:mt-16 lg:mt-20">
@@ -48,3 +63,27 @@
     </div>
   </center>
 </main>
+
+<style>
+  .waviy {
+    position: relative;
+    -webkit-box-reflect: below -20px linear-gradient(transparent, rgba(0, 0, 0, 0.2));
+    letter-spacing: -7px;
+  }
+  .waviy span {
+    position: relative;
+    display: inline-block;
+    animation: waviy 2s infinite;
+    animation-delay: calc(0.15s * var(--i));
+  }
+  @keyframes waviy {
+    0%,
+    40%,
+    100% {
+      transform: translateY(0);
+    }
+    20% {
+      transform: translateY(-8px);
+    }
+  }
+</style>
