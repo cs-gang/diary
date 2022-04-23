ec1989e98b3c3b482d0141e481ef41602ba58739
anand2312 <40204976+anand2312@users.noreply.github.com>
2022-04-23 13:17:37 +0400

    chore: remove unnecessary fields from config

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
 import sveltePreprocess from 'svelte-preprocess';
-import cloudflare from '@sveltejs/adapter-cloudflare';
-//import { sveltePreprocess } from 'svelte-preprocess/dist/autoProcess';
+import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
-
-  kit: {
-    adapter: adapter(),
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
-  }
-};
 
 export default {
   preprocess: sveltePreprocess(),
   kit: {
-    target: '#svelte',
-    adapter: cloudflare({
-      // any esbuild options
-    })
+    adapter: adapter()
   }
 };
